01c562e2890a55e49b5f756482ad03e6cb57856e
ilshat16 <illliat@mail.ru>
2023-11-08 19:06:28 +0300

    realize csrf token

--- src/main/java/ru/ilshat/firstSecurityApp/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/ilshat/firstSecurityApp/FirstSecurityApp/config/SecurityConfig.java
@@ -29,8 +29,6 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
-		.csrf(csrf -> csrf
-				.disable())
 		.authorizeHttpRequests(authorize -> authorize
 	            .requestMatchers("/hello").authenticated()
 	            .anyRequest().permitAll())
