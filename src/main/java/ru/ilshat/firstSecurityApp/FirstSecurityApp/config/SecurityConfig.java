3377b45fbfb93df9c54acdffd4a362f6cb35f018
ilshat16 <illliat@mail.ru>
2023-11-08 10:11:26 +0300

    delete AuthProviderImpl

--- src/main/java/ru/ilshat/firstSecurityApp/FirstSecurityApp/config/SecurityConfig.java
+++ src/main/java/ru/ilshat/firstSecurityApp/FirstSecurityApp/config/SecurityConfig.java
@@ -1,24 +1,32 @@
 package ru.ilshat.firstSecurityApp.FirstSecurityApp.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import ru.ilshat.firstSecurityApp.FirstSecurityApp.security.AuthProviderImpl;
+import ru.ilshat.firstSecurityApp.FirstSecurityApp.services.PersonDetailsService;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfiguration{
 	
-	private final AuthProviderImpl authProviderImpl;
-	
+	private PersonDetailsService personDetailsService;
+
 	@Autowired
-	public SecurityConfig(AuthProviderImpl authProviderImpl) {
-		this.authProviderImpl = authProviderImpl;
+	public SecurityConfig(PersonDetailsService personDetailsService) {
+		this.personDetailsService = personDetailsService;
 	}
 
 
-	protected void configure(AuthenticationManagerBuilder auth) {
-		auth.authenticationProvider(authProviderImpl);
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(personDetailsService);
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		return NoOpPasswordEncoder.getInstance();
 	}
 }
